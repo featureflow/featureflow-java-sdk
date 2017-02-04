99b13c9fdc18ed45e0e8e915850b1bb935f3d8f4
Oliver Oldfield-Hodge <oliver.oldfield-hodge@industrieit.com>
2017-02-04 18:12:47 +1100

    Websocket updated for feature and feature controls. Missing delete on environment. tbc

--- mvn-deploy-to-central.sh
+++ mvn-deploy-to-central.sh
@@ -1,4 +1,9 @@
 #!/usr/bin/env bash
+# eg ./mvn-deploy-to-cenral.sh 0.0.2-SNAPSHOT 0.0.3-SNAPSHOT
 #-DgenerateBackupPoms=false
 mvn versions:set -DnewVersion=$1
 mvn versions:commit
+mvn deploy
+mvn versions:set -DnewVersion=$2
+mvn versions:commit
+
