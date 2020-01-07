554abd202d127dae643b3a8cea4b14528a33cdba
oliveroldfield <oliver@featureflow.io>
2020-01-07 13:23:57 +1100

    Update FeatureflowUserLookupProvider.java

--- src/main/java/io/featureflow/client/FeatureflowUserLookupProvider.java
+++ src/main/java/io/featureflow/client/FeatureflowUserLookupProvider.java
@@ -1,7 +1,7 @@
 package io.featureflow.client;
 
 /**
- * Implement the user provider to provide featureflow is a user and attributes
+ * User provider to provide featureflow with a user and attributes
  */
 public interface FeatureflowUserLookupProvider {
     FeatureflowUser getUser(String userId);
