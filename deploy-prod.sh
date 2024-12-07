42b22fb8aa3674656aac307373d259ab8a1b57ad
oliveroldfield-hodge <oliver@featureflow.io>
2024-12-07 13:12:15 +0400

    FLOW-1020 Update deployment Key

--- deploy-prod.sh
+++ deploy-prod.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
-echo Deploy to Production
-openssl aes-256-cbc -d -in codesigning.asc.enc -out codesigning.asc -k $ENCKEY
+openssl aes-256-cbc -d -in codesigning.asc.enc -out codesigning.asc -k ${ENCKEY} -pbkdf2
 gpg --fast-import codesigning.asc
-mvn deploy -P sign,build-extras --settings settings.xml
+./mvnw deploy -P sign,build-extras --settings settings.xml
