5ce1474eddacf96c2f42dff4deeb11cd7e5a4ab9
oliveroldfield-hodge <oliver@featureflow.io>
2024-12-08 05:36:45 +0400

    FLOW-1020 Update deployment versions

--- deploy-prod.sh
+++ deploy-prod.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 openssl aes-256-cbc -d -in codesigning.asc.enc -out codesigning.asc -k ${ENCKEY} -pbkdf2
 gpg --fast-import codesigning.asc
-./mvnw deploy -P sign,build-extras --settings settings.xml
+mvn deploy -P sign,build-extras --settings settings.xml
